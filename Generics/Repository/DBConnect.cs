81bcdf527b753067259e068a43ae999d0ffa11ef
VINICIUS GABRIEL MACANEIRO <vinicius.macaneiro@gmail.com>
2023-10-06 21:02:48 -0300

    Não pode ter senha, po

--- Generics/Repository/DBConnect.cs
+++ Generics/Repository/DBConnect.cs
@@ -14,7 +14,7 @@ namespace Business.Repository
         static string dataSource = @"localhost\SQLEXPRESS";
 
         static string userID = "sa";
-        static string password = "Senac@2021";
+        static string password = "Bolinha";
 
 
         public static string Connect()
