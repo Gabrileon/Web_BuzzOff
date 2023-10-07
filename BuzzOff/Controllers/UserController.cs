03c007bd961d1e9c08dd992ede53cca8e9d6dec6
gabriel.montibeler <gabriel.montibeler@BUE0001D021>
2023-10-06 21:27:25 -0300

    Corrigindo controller

--- BuzzOff/Controllers/UserController.cs
+++ BuzzOff/Controllers/UserController.cs
@@ -5,14 +5,12 @@ namespace BuzzOff.Controllers
     public class UserController : Controller
     {
         public IActionResult Index()
-        {
-            ViewBag.ShowNavbar = false;
+        {            
             return View();
         }
         public IActionResult Login()
         {
-
             return View();
-        }
+        }           
     }
 }
