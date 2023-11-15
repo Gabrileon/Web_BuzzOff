cf50bf5b96a55d03ae838319929625b251716f80
VINICIUS GABRIEL MACANEIRO <vinicius.macaneiro@gmail.com>
2023-11-14 21:23:24 -0300

    Sistema de Login Implementado, dessa vez utilizando Session

--- BuzzOff/Program.cs
+++ BuzzOff/Program.cs
@@ -3,6 +3,48 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).
+    AddCookie(options =>
+    {
+        options.LoginPath = "/Login/Index";
+        //options.AccessDeniedPath = "/Login/DeniedAccess";
+    }
+    );
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Administrator", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireAssertion(context =>
+        {
+            var user = context.User;
+            return user.Claims.Any(property => property.Type == "AccessLevel" && property.Value == "Administrator");
+        });
+    });
+    options.AddPolicy("Agent", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireAssertion(context =>
+        {
+            var user = context.User;
+            return user.Claims.Any(property => property.Type == "AccessLevel" && property.Value == "Agent");
+        });
+    });
+    options.AddPolicy("Common", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireAssertion(context =>
+        {
+            var user = context.User;
+            return user.Claims.Any(property => property.Type == "AccessLevel" && property.Value == "Common");
+        }); ;
+    });
+}
+    );
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
