edfa70e6c974431e11eff5d92b834c09ff5fd56a
VINICIUS GABRIEL MACANEIRO <vinicius.macaneiro@gmail.com>
2023-11-16 21:02:57 -0300

    Recuperando IDenunciation

---
+++ Common/Interfaces/IDenunciation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    public interface IDenunciation
+    {
+        public int Id { get; set; }
+        public int IdInformer { get; set; }
+        public int IdAddress { get; set; }
+        public DateTime DataDenunciation { get; set; }
+        public byte[] media { get; set; }
+        public bool IsAnswered { get; set; }
+    }
+}
