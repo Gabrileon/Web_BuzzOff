4145462039ee2f63f578c22067dac7e58ebc3c39
Gabiru <Gabiru@DESKTOP-IV69V16>
2023-11-23 23:00:05 -0300

    Count focos alterada

---
+++ BuzzOff/Models/CountFocusModel.cs
@@ -0,0 +1,25 @@
+﻿using Common.Interfaces;
+
+namespace BuzzOff.Models
+{
+    public class CountFocusModel
+    {
+        public CountFocusModel()
+        {
+
+        }
+        public CountFocusModel(int count, string neighborhood)
+        {
+            Count = count;
+            Neighborhood = neighborhood;
+        }
+
+        public int Count { get; set; }
+        public string Neighborhood { get; set; }
+    }
+
+    public class AmountFocusModel : IAmountFocus
+    {
+        public int Value { get; set; }
+    }
+}
