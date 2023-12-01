c2f203c1ccd715268090cae075d99d22dc04768a
gabriel.montibeler <gabriel.montibeler@BUE0001D021>
2023-11-30 21:31:44 -0300

    Corrigindo layout Commom user list

---
+++ BuzzOff/Models/VisitsModel.cs
@@ -0,0 +1,28 @@
+﻿using Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuzzOff.Models
+{
+    public class VisitModel: IVisit
+    {
+        public VisitModel(int id, int idAgent, int idDenunciation, DateTime dateVisit, string assessment)
+        {
+            Id = id;
+            IdAgent = idAgent;
+            IdDenunciation = idDenunciation;
+            DateVisit = dateVisit;
+            Assessment = assessment;
+        }
+
+        public int Id { get; set; }
+        public int IdAgent { get; set; }
+        public int IdDenunciation { get; set; }
+        public DateTime DateVisit { get; set; }
+        public string Assessment { get; set; }
+
+    }
+}
