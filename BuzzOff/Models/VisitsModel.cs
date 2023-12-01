4a08b057baa42f1fd4ef611ae1833bffc88e1a12
gabriel.montibeler <gabriel.montibeler@BUE0001D021>
2023-11-30 21:32:50 -0300

    VisitsModel ainda não existe

---
+++ BuzzOff/Models/VisitsModel.cs
@@ -0,0 +1,15 @@
+﻿using Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuzzOff.Models
+{
+    public class VisitsModel
+    {
+        public List<IVisit> Visits { get; set; } = new List<IVisit>();
+
+    }
+}
