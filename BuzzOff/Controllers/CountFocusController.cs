392b6bb42b7c0ed5ff5dd6eacf207dc736f79651
gabriel.montibeler <gabriel.montibeler@BUE0001D021>
2023-12-04 21:07:54 -0300

    Pronto

--- BuzzOff/Controllers/CountFocusController.cs
+++ BuzzOff/Controllers/CountFocusController.cs
@@ -3,6 +3,8 @@ using Business.Repository.DAO;
 using BuzzOff.Models;
 using Common.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
 namespace BuzzOff.Controllers
 {
     public class CountFocusController : Controller
@@ -11,52 +13,27 @@ namespace BuzzOff.Controllers
         {
             ViewBag.Message = "Mapa de Focos";
             int countTotal = CountFocusDAO.AmountByErradicated(false);
-            var model = new CountFocusesModel();            
+            var model = new CountFocusesModel();
             model.TotalFocus = countTotal;
             foreach (var focus in CountFocusDAO.CountAllErraticated(false))
             {
                 model.CountFocus.Add(new CountFocusModel(focus));
             }
-            // Popule o modelo com dados, como você já está fazendo
-
-            // Criar lista de features GeoJSON para cada bairro
-            var neighborhoodsGeoJSON = new List<object>();
-
-            foreach (var focus in model.CountFocus)
+            var data = new List<MapParameterModel>();
+            foreach (var bairro in CoordinateDAO.GetAll())
             {
-                var coordinates = GetCoordinatesForNeighborhood(focus.Neighborhood);
-
-                var feature = new
+                data.Add(new MapParameterModel()
                 {
-                    type = "Feature",
-                    properties = new
-                    {
-                        name = focus.Neighborhood,
-                        focusCount = focus.Counts
-                    },
-                    geometry = new
-                    {
-                        type = "Point",
-                        coordinates = coordinates
-                    }
-                };
-
-                neighborhoodsGeoJSON.Add(feature);
+                    Id = bairro.Id,
+                    Nome = bairro.Neighborhood,
+                    Latitude = bairro.Latitude.ToString(new CultureInfo("en-US")),
+                    Longitude = bairro.Longitude.ToString(new CultureInfo("en-US")),
+                    Count = CountFocusDAO.CountByErraticatedAndNeighborhood(false, bairro.Neighborhood).Counts,
+                });
             }
 
-            // Converter a lista para um array para uso no JavaScript
-            ViewBag.NeighborhoodsGeoJSON = Newtonsoft.Json.JsonConvert.SerializeObject(neighborhoodsGeoJSON);
-
+            ViewBag.MapData = data;
             return View(model);
-        }
-
-        // Método para obter as coordenadas do bairro usando o OpenCage Geocoding API
-        private double[] GetCoordinatesForNeighborhood(string neighborhoodName)
-        {
-            // Substitua pelo código real para chamar o serviço de geocodificação
-            // Aqui, estou usando coordenadas de exemplo para fins ilustrativos.
-            // Certifique-se de implementar essa chamada de maneira adequada.
-            return new double[] { -26.8982, -49.0759 }; // Coordenadas de exemplo para Blumenau
         }
     }
 }
